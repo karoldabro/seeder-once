67eefe707bfd25989fc6329208ec9ef9c2afe54f
Karol Dąbrowski <karol_dabrowski@interia.eu>
2020-02-10 20:08:53 +0100

    Refactor SeederOnce trait

--- src/SeederOnce.php
+++ src/SeederOnce.php
@@ -2,14 +2,43 @@
 
 namespace Kdabrow\SeederOnce;
 
+use Illuminate\Support\Arr;
 use InvalidArgumentException;
-use Kdabrow\SeederOnce\Tests\NotImplementedException;
 
 trait SeederOnce
 {
-    public function callOnce()
+    /**
+     * Seed the given connection from the given path.
+     *
+     * @param  array|string  $class
+     * @param  bool  $silent
+     * @return $this
+     */
+    public function call($class, $silent = false)
     {
-        throw new NotImplementedException();
+        $classes = Arr::wrap($class);
+
+        foreach ($classes as $class) {
+            $seeder = $this->resolve($class);
+
+            $name = get_class($seeder);
+
+            if ($silent === false && isset($this->command)) {
+                $this->command->getOutput()->writeln("<comment>Seeding:</comment> {$name}");
+            }
+
+            $startTime = microtime(true);
+
+            $seeder->__invoke();
+
+            $runTime = round(microtime(true) - $startTime, 2);
+
+            if ($silent === false && isset($this->command)) {
+                $this->command->getOutput()->writeln("<info>Seeded:</info>  {$name} ({$runTime} seconds)");
+            }
+        }
+
+        return $this;
     }
 
     /**
@@ -21,21 +50,12 @@ trait SeederOnce
      */
     public function __invoke()
     {
-        if (method_exists($this, 'run')) {
-            $this->runOrCall();
+        if (!method_exists($this, 'run')) {
+            throw new InvalidArgumentException('Method [run] missing from ' . get_class($this));
         }
 
-        if (method_exists($this, 'runOnce')) {
-            $this->runOrCall('runOnce');
-        }
-
-        throw new InvalidArgumentException('Method [run] or method [runOnce] are missing from ' . get_class($this));
-    }
-
-    private function runOrCall(string $methodName = 'run')
-    {
         return isset($this->container)
-            ? $this->container->call([$this, $methodName])
-            : $this->$methodName();
+            ? $this->container->call([$this, 'run'])
+            : $this->run();
     }
 }
--- tests/Unit/SeederTraitTest.php
+++ tests/Unit/SeederTraitTest.php
@@ -2,35 +2,8 @@
 
 namespace Kdabrow\SeederOnce\Tests\Unit;
 
-use Mockery;
-use InvalidArgumentException;
 use Kdabrow\SeederOnce\Tests\TestCase;
-use Kdabrow\SeederOnce\Tests\Unit\Mocks\WithRunMethodSeederMock;
-use Kdabrow\SeederOnce\Tests\Unit\Mocks\WithoutMethodsSeederMock;
-use Kdabrow\SeederOnce\Tests\Unit\Mocks\WithRunOnceMethodSeederMock;
 
 class SeederTraitTest extends TestCase
 {
-    public function test_if_methods_run_or_run_once_do_not_exists_then_throw_exception()
-    {
-        $this->expectException(InvalidArgumentException::class);
-
-        $seeder = new WithoutMethodsSeederMock();
-
-        $seeder();
-    }
-
-    public function test_if_method_run_once_can_be_called()
-    {
-        $seeder = new WithRunOnceMethodSeederMock();
-
-        $this->assertTrue($seeder());
-    }
-
-    public function test_if_method_run_can_be_called()
-    {
-        $seeder = new WithRunMethodSeederMock();
-
-        $this->assertTrue($seeder());
-    }
 }
